370ca918e529740da8aad22a036351677ec49b24
Charles <charles.trangay11@gmail.com>
2021-03-20 12:17:11 -0400

    login with routing implemented

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Login from './Login';
 import HomePage from "./HomePage";
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-//comment 
+import DashBoard from "./DashBoard"
+import Recipe from "./Recipe"
+
 const App = () => {
   const [user, setUser] = useState('');
   const [email, setEmail] = useState('');
@@ -82,23 +84,50 @@ useEffect(() => {
 }, [])
 
 return (
+  /* Exact path = "/" is the first page you see like facebook. Ask you to login/signup.
+
+     <Route path = "nameOfPath">
+      <nameOfPath/>
+     </Route>
+
+     Note: Replace nameOfPath with name of exact javascript file name, for example - DashBoard.
+     Also remember to put this [import nameOfPath from "./nameOfPath"] all the way ontop of this App.js file,
+     for example - import DashBoard from "./DashBoard"
+
+     Note 2: Then in any javascript file aside from App.js you could put this [<Link to = "/nameOfPath">nameOfPath</Link>]
+     This adds a button to go to any javascript file that you added below in the Routes.
+     Also remember to add [import {Link} from "react-router-dom"] in the top of the javascript file 
+     to make the Link tag work. Look at HomePage.js for an example. 
+  */
   <div className="App">
-    {user ? (
-      <HomePage handleLogout={handleLogout} />
-    ) : (
-      <Login 
-        email={email} 
-        setEmail={setEmail} 
-        password={password} 
-        setPassword={setPassword} 
-        handleLogin={handleLogin} 
-        handleSignup={handleSignup} 
-        hasAccount={hasAccount}
-        setHasAccount={setHasAccount}
-        emailError={emailError}
-        passwordError={passwordError}
-    />
-    )}
+    <Router>
+      <Switch>
+      <Route exact path = "/WeCook"> 
+          {user ? (
+            <HomePage handleLogout = {handleLogout}/>
+          ): (
+            <Login 
+            email={email} 
+            setEmail={setEmail} 
+            password={password} 
+            setPassword={setPassword} 
+            handleLogin={handleLogin} 
+            handleSignup={handleSignup} 
+            hasAccount={hasAccount}
+            setHasAccount={setHasAccount}
+            emailError={emailError}
+            passwordError={passwordError}
+            />
+            )}
+      </Route>
+      <Route path = "/DashBoard"> 
+        <DashBoard/>
+      </Route>
+      <Route path = "/Recipe">
+        <Recipe/>
+      </Route>
+      </Switch>
+    </Router>
   </div>
   );
 };
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import {Link} from "react-router-dom"
 const HomePage = ({handleLogout}) => {
     return(
         <section className = "HomePage">
             <nav>
                 <h2>Welcome</h2>
                 <button onClick={handleLogout}>Logout</button>
+                Go to DashBoard? <Link to = "/DashBoard">DashBoard</Link>
             </nav>
         </section>
     )
