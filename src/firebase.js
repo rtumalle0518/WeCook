185e1a1eefa2be57acc110319e135114a9bb3230
rtumalle0518 <rtumalle9700@gmail.com>
2021-03-24 18:15:48 -0400

    fixed firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyDVOmGXBvjf-4Nm8xs1pNWjSVVNn6y6zmc",
+    authDomain: "auth-development-94c42.firebaseapp.com",
+    databaseURL: "https://auth-development-94c42-default-rtdb.firebaseio.com/",
+    projectId: "auth-development-94c42",
+    storageBucket: "auth-development-94c42.appspot.com",
+    messagingSenderId: "1091844701781",
+    appId: "1:1091844701781:web:c073c807a8b763fe849edc"
 })
 
 export const auth = app.auth()
