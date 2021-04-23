431d61ef0d1860e8042e801fee2ecab7751628ca
rtumalle0518 <43913227+rtumalle0518@users.noreply.github.com>
2021-04-23 17:43:44 -0400

    took out comments from ViewAllRecipes

--- src/components/ViewAllRecipes.js
+++ src/components/ViewAllRecipes.js
@@ -12,9 +12,7 @@ var uid;
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         if (user != null) {
-            uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                             // this value to authenticate with your backend server, if
-                             // you have one. Use User.getToken() instead.
+            uid = user.uid;
           }
     } else {
       // No user is signed in.
